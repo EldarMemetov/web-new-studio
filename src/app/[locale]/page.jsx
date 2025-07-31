30dcd28d975d809cf25e262f420f41d026866058
EldarMemetov <eldarm9347@gmail.com>
2025-07-31 08:40:02 +0200

    update

--- src/app/[locale]/page.jsx
+++ src/app/[locale]/page.jsx
@@ -1,45 +1,34 @@
 import dynamic from 'next/dynamic';
+
 import { ReviewsSection } from '@/modules/GetReview/Components/ReviewsSection/ReviewsSection';
 import { GetReviews } from '@/services/api';
+
 import FetchServices from '@/modules/FetchServices/FetchServices';
+
 import GetBusinessSolutions from '@/modules/GetBusinessSolutions/GetBusinessSolutions';
 import BrandTransformation from '@/modules/BrandTransformation/BrandTransformation';
 import ToggleQuestions from '@/modules/ToggleQuestions/ToggleQuestions';
-import DisplayCompanyFacts from '@/modules/DisplayCompanyFacts/DisplayCompanyFacts';
-import FeedbackWrapper from '@/shared/FeedbackWrapper/FeedbackWrapper';
-import HeroSection from '@/modules/HeroSection/HeroSection';
 import s from './page.module.scss';
 
-import { initServerI18n } from '@/i18n/utils/serverI18n';
-
+import HeroSection from '@/modules/HeroSection/HeroSection';
+import DisplayCompanyFacts from '@/modules/DisplayCompanyFacts/DisplayCompanyFacts';
+import FeedbackWrapper from '@/shared/FeedbackWrapper/FeedbackWrapper';
 const ReviewsList = dynamic(
   () => import('@/modules/GetReview/Components/ReviewsList/ReviewsList')
 );
 const Portfolio = dynamic(() => import('@/modules/Portfolio/Portfolio'));
 const IdeasHome = dynamic(() => import('@/modules/IdeasHome/IdeasHome'));
-
 export default async function Home({ params: rawParams }) {
   const params = await rawParams;
   const availableLocales = ['en', 'ua', 'de'];
   const locale = availableLocales.includes(params?.locale)
     ? params.locale
     : 'en';
-
-  const { t } = await initServerI18n(locale, ['heroSection']);
-
   const reviews = await GetReviews();
 
   return (
     <div className={s.container}>
-      <HeroSection
-        title={t('title')}
-        description={t('description')}
-        subtitle={t('subtitle')}
-        subtitleMobile={t('subtitleMobile')}
-        linkPortfolio={t('linkPortfolio')}
-        goToPortfolio={t('goToPortfolio') || 'Go to portfolio'}
-        buttonText={t('button')}
-      />
+      <HeroSection locale={locale} />
       <DisplayCompanyFacts locale={locale} />
       <FetchServices />
       <GetBusinessSolutions locale={locale} />
--- src/app/[locale]/web-development/page.jsx
+++ src/app/[locale]/web-development/page.jsx
@@ -1,7 +1,7 @@
 import dynamic from 'next/dynamic';
 import s from './webDevelopment.module.scss';
 import ToggleQuestions from '@/modules/ToggleQuestions/ToggleQuestions';
-import { initServerI18n } from '@/i18n/utils/serverI18n';
+
 import WebWhyChoose from '@/modules/WebWhyChoose/WebWhyChoose';
 import WebPortfolio from '@/modules/WebPortfolio/WebPortfolio';
 import OrDevelopment from '@/modules/OrDevelopment/OrDevelopment';
@@ -18,18 +18,10 @@ export default async function WebDevelopment({ params: rawParams }) {
   const locale = availableLocales.includes(params?.locale)
     ? params.locale
     : 'en';
-  const { t } = await initServerI18n(locale, ['webHero']);
+
   return (
     <div className={s.container}>
-      <HeroSection
-        title={t('title')}
-        description={t('description')}
-        subtitle={t('subtitle')}
-        subtitleMobile={t('subtitleMobile')}
-        linkPortfolio={t('linkPortfolio')}
-        goToPortfolio={t('goToPortfolio') || 'Go to portfolio'}
-        buttonText={t('button')}
-      />
+      <HeroSection locale={locale} namespace="webHero" />
       <WebDisplayCompany locale={locale} />
       <EffectiveSolutions locale={locale} />
       <WebPortfolio locale={locale} />
