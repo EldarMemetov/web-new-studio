ec83b9858842b925a4c0345cd5ffe6c0006972b2
EldarMemetov <eldarm9347@gmail.com>
2025-08-03 13:00:30 +0200

    add footer

--- src/modules/Footer/Footer.jsx
+++ src/modules/Footer/Footer.jsx
@@ -6,31 +6,57 @@ import Logo from '@/shared/Logo/Logo';
 import NavMenu from '../Header/NavMenu/NavMenu';
 import { SocialLinks } from '../Header/SocialLinks/SocialLinks';
 import Link from 'next/link';
+
 import { useTranslation } from 'react-i18next';
 
 export default function Footer() {
   const { t, i18n } = useTranslation(['footer']);
   const locale = i18n.language;
   const currentYear = new Date().getFullYear();
-
   return (
     <footer className={s.footer}>
       <Container>
-        <div className={s.wrapper}>
-          <div className={s.logo}>
-            <Logo variant="footer" />
+        <div className={s.padding}>
+          <div className={s.containerFooter}>
+            <div className={s.logoMobile}>
+              <Logo variant="footer" />
+            </div>
+
+            <NavMenu variant="footer" locale={locale} />
+            <div className={s.social}>
+              <SocialLinks />
+            </div>
           </div>
-          <NavMenu variant="footer" locale={locale} />
-          <div className={s.social}>
-            <SocialLinks />
-          </div>
-          <div className={s.bottom}>
+          <div className={s.privacyAndRights}>
             <p className={s.rights}>
               {t('rights').replace('{year}', currentYear)}
             </p>
-            <Link href={`/${locale}/privacy-policy`} className={s.privacyLink}>
-              {t('privacy')}
-            </Link>
+            <div className={s.footerLinks}>
+              <Link
+                href={`/${locale}/privacy-policy`}
+                className={s.privacyLink}
+              >
+                {t('privacy')}
+              </Link>
+            </div>
+          </div>
+
+          <div className={s.desktop}>
+            <div className={s.desktopLogo}>
+              <Logo variant="footer" />
+              <p className={s.rights}>
+                {t('rights').replace('{year}', currentYear)}
+              </p>
+            </div>
+            <div className={s.desktopSocial}>
+              <SocialLinks />
+              <Link
+                href={`/${locale}/privacy-policy`}
+                className={s.privacyLink}
+              >
+                {t('privacy')}
+              </Link>
+            </div>
           </div>
         </div>
       </Container>
